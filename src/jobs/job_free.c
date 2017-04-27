3e77cb0ffd8159b0c063979e90100295a84d486f
Quentin Le Bevillon <qle-bevi@student.42.fr>
2017-04-27 17:16:05 +0200

    fix

--- src/jobs/job_free.c
+++ src/jobs/job_free.c
@@ -6,12 +6,13 @@
 /*   By: qle-bevi <qle-bevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 16:52:46 by qle-bevi          #+#    #+#             */
-/*   Updated: 2017/04/27 17:10:58 by qle-bevi         ###   ########.fr       */
+/*   Updated: 2017/04/27 17:15:36 by qle-bevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "job.h"
 #include "libft.h"
+#include <stdlib.h>
 
 void	job_free(t_job *job)
 {
