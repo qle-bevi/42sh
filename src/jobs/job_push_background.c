4d6b9bf4666f8d6653c6a9435bee9c922b973f36
Quentin Le Bevillon <qle-bevi@student.42.fr>
2017-04-27 16:37:42 +0200

    switched headers

--- src/jobs/job_push_background.c
+++ src/jobs/job_push_background.c
@@ -6,12 +6,13 @@
 /*   By: qle-bevi <qle-bevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 09:52:22 by qle-bevi          #+#    #+#             */
-/*   Updated: 2017/04/13 09:52:22 by qle-bevi         ###   ########.fr       */
+/*   Updated: 2017/04/27 16:32:07 by qle-bevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "job.h"
 #include <unistd.h>
+#include <signal.h>
 
 void	job_push_background(t_job *job)
 {
