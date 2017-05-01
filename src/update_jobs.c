7696baf4fdf2127e87dd52b1224ad94b96464ebe
Quentin Le Bevillon <qle-bevi@student.42.fr>
2017-05-01 18:26:19 +0200

    fix

--- src/update_jobs.c
+++ src/update_jobs.c
@@ -6,7 +6,7 @@
 /*   By: qle-bevi <qle-bevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/29 09:50:23 by qle-bevi          #+#    #+#             */
-/*   Updated: 2017/05/01 16:44:39 by qle-bevi         ###   ########.fr       */
+/*   Updated: 2017/05/01 18:25:23 by qle-bevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static void	handle_done(t_shell *sh, t_job *job, t_cmd *cmd)
 	}
 	if (cmd->ret == 130)
 		job_terminate(job, 130);
-	else if (cmd->ret || !cmd->children)
+	else if (!cmd->children)
 	{
 		cmd_terminate(job->current_cmd, cmd->ret);
 		job_next_cmd(job);
